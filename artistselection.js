c843d7f3d91502781bfca171fd91af6f5083afed
borlingball15 <ananya.borle@gmail.com>
2024-04-17 01:00:17 -0400

    json calls

--- artistselection.js
+++ artistselection.js
@@ -1,9 +1,8 @@
-// Load the JSON file
-fetch('xxl_freshman.json')
+fetch('https://raw.githubusercontent.com/dariusk/corpora/27c133b7b3adb318582103a1895a2c9b1514ea54/data/music/xxl_freshman.json')
     .then(response => response.json())
     .then(data => {
         const selectedYear = localStorage.getItem("selectedYear");
-        document.getElementById("yearDisplay").innerText = selectedYear;
+        document.getElementById("selectedYear").innerText = selectedYear;
 
         const artists = data[selectedYear].map(artist => artist.toUpperCase());
 
@@ -11,8 +10,24 @@ fetch('xxl_freshman.json')
         artists.forEach(artist => {
             const listItem = document.createElement("li");
             listItem.textContent = artist;
-            listItem.style.fontFamily = "Helvetica Neue, Helvetica, Arial, sans-serif";
+            listItem.style.fontSize = "30px";
+            listItem.style.opacity = "0.8";
+            listItem.style.transition = "opacity 0.3s";
             listItem.style.fontWeight = "bold";
+            listItem.addEventListener("mouseover", () => {
+                listItem.style.opacity = "1";
+            });
+            listItem.addEventListener("mouseout", () => {
+                listItem.style.opacity = "0.8";
+            });
             artistListElement.appendChild(listItem);
         });
+
+        // Aspace
+        const spacer = document.createElement("span");
+        spacer.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"; 
+        artistListElement.parentNode.insertBefore(spacer, artistListElement);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
     });
